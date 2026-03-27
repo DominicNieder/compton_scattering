65172a21e26c016a7f3bb656a3ce0dd384c3b825
DominiNieder <dominic.nieder@gmail.com>
2026-03-27 16:13:56 +0100

    code: prep for ana

--- analysis-code/handling_data.py
+++ analysis-code/handling_data.py
@@ -2,3 +2,21 @@
 #            Handling data: read and write
 #------------------------------------------------------
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def load_spectrum(filepath):
+    """Returns DataFrame with columns: channel, counts"""
+    df = pd.read_csv(filepath, sep='/t', comment='#',
+                     names=['counts'])
+    return df
+
+if __name__ == '__main__':
+    test_data_path = "../data/test_data/1-co-wed-LA5.TKA"
+    test_data = load_spectrum(test_data_path)
+    print(test_data)
+    channels =np.arange(len(test_data.index))
+    counts = test_data['counts'].to_numpy()
+    plt.plot(channels, counts)
+    plt.savefig("../figures/test_data_plot.png")
