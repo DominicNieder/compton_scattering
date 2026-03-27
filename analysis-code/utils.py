6d6adcf714a1617fc24a17669830f4628c7abf18
DominiNieder <dominic.nieder@gmail.com>
2026-03-27 20:15:01 +0100

    code: initialisation of .json

--- analysis-code/utils.py
+++ analysis-code/utils.py
@@ -7,12 +7,13 @@ import json
 from datetime import datetime
 
 
-def log_file(folder, filename, description):
+def keep_log(folder, target, description):
     """
-    Add or update an entry in orientation.json
+    Add or update an entry in orientation.json;
+    The entry type is 'dict';
     ---
     folder:         the target folder of object
-    filename:       the target filename to be described
+    target:       the target filename to be described
     description:    the suitable description of filename
     """
     log_path = os.path.join(folder, "orientation.json")
@@ -24,10 +25,20 @@ def log_file(folder, filename, description):
         index = {}
     
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
-    index[filename] = {
+    index[target] = {
                         "description": description,
                         "update": timestamp
                     }
     
     with open(log_path, "w") as f:
         json.dump(index, f, indent=2)
+
+
+
+if __name__ == "__main__":
+    results_dir = "../results"
+    test_target = "test result" 
+    test_mu, test_dmu = (6, 0.5) 
+    test_result = f"mu={test_mu:.1f}+-{test_dmu:.1f}keV"
+    keep_log(results_dir, test_target, test_result)
+    
